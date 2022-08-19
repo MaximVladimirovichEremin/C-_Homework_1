df8dc360c4afc09b8ec9f8079fd274b8b51f0d32
Maxim_Eremin <eremin.msk@yandex.ry>
2022-08-19 21:09:48 +0300

    2 задача

--- C#_Homework_1/Program.cs
+++ C#_Homework_1/Program.cs
@@ -5,7 +5,7 @@ int num1;
 
 int num2;
 
-// Просим ввести числа 
+// Просим пользователя ввести числа 
 Console.Write("Введите первое число : ");
 num1 = Convert.ToInt32(Console.ReadLine());
 
@@ -21,4 +21,43 @@ else
     Console.WriteLine("Максимальное число " + num2); 
 }
 */
+
+/*
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Вводим переменные 
+int num1;
+
+int num2;
+
+int num3;
+//Добовляем переменную макс 
+int max;
+
+// Просим пользователя ввести числа 
+Console.Write("Введите первое число : ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+// Сравниваем 1и 2  число, запоминаем максимальное
+if (num1 > num2){
+    max = num1;
+}
+else
+    max = num2;
+
+// Сравниваем максимальное с 3 числов, выводим максимальное на консоль 
+if (num3 > max){
+    max = num3;
+    Console.WriteLine("Максимальное число " + max);
+}
+else
+    Console.WriteLine("Максимальное число " + max);
+*/
+
 
